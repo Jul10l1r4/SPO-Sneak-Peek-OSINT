184d98cb3496bf79f5f0725e2e90e51a12916d06
victor <victordequeiroz37@gmail.com>
2018-05-08 15:01:28 -0300

    alterated structure for investigation

--- main.py
+++ main.py
@@ -49,40 +49,46 @@ spo = Flask(__name__, template_folder="templates", static_folder="static")
 # on apache for example, use this key for wsgi
 spo.secret_key = 'ˆˆß∂……å¬ßøøøø∑ßˆˆß∆˚¬˜˜≤'
 
-#instance of data access object
+# instance of data access object
 dao = spoDAO.SpoDAO()
-#instance of Whois
+# instance of Whois
 whois = Whois.Whois()
-#instance of cnpj api
+# instance of cnpj api
 cnpj = CNPJ.CNPJ()
-#instance of jurisprudence
+# instance of jurisprudence
 lawyer = Jurisprudence.Jurisprudence()
-#instance of subdomains
+# instance of subdomains
 subdomain = Subdomain.Subdomains()
 
-#for timeout session on 2 minutes
+
+# for timeout session on 2 minutes
 @spo.before_request
 def make_session_permanent():
     session.permanent = True
     spo.permanent_session_lifetime = timedelta(minutes=15)
 
-#for logout
+
+# for logout
 @spo.route("/exit")
 def exit():
     session.pop('user', None)
     return redirect(url_for("login"))
 
-#errors
+
+# errors
 @spo.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
 @spo.errorhandler(403)
 def page_not_found(e):
     return render_template('403.html'), 403
+
+
 @spo.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
-
 
 
 """
@@ -95,9 +101,11 @@ Function that is responsible for managing login and
 user creation for the first access by opening the cookie 
 session and dealing with sqli attacks ex: a ' or 1 = 1#
 """
-@spo.route("/", methods=['GET','POST'])
+
+
+@spo.route("/", methods=['GET', 'POST'])
 def login():
-    #on first access redirect to firstLogin
+    # on first access redirect to firstLogin
     if dao.testFirstAccess() == True:
         return redirect('wellcome')
 
@@ -108,9 +116,9 @@ def login():
             # recive password from html
             getPasswd = request.form.get('password')
 
-            #test login
-            if dao.testLogin(getUser,getPasswd) == True:
-                #open a session
+            # test login
+            if dao.testLogin(getUser, getPasswd) == True:
+                # open a session
                 session['user'] = getUser
                 session['isAuthenticated'] = True
                 session['id'] = dao.getIDUser(getUser)
@@ -118,16 +126,18 @@ def login():
             else:
                 return render_template("passwordFail.html")
 
-        #render login.html
+        # render login.html
         return render_template("login.html")
 
-#for first login
+
+# for first login
 @spo.route("/wellcome")
 def wellcome():
-    #wellcome screen
+    # wellcome screen
     return render_template('wellcome.html')
 
-@spo.route("/firstLogin", methods=['GET','POST'])
+
+@spo.route("/firstLogin", methods=['GET', 'POST'])
 def firstLogin():
     if request.method == 'POST':
         # recive user from html
@@ -140,7 +150,6 @@ def firstLogin():
     return render_template("firstLogin.html")
 
 
-
 """
 +-------------------------------------------------+
 |                                                 |
@@ -149,61 +158,75 @@ def firstLogin():
 +-------------------------------------------------+
 Function that is responsible for principal page
 """
-@spo.route("/dashboard", methods=['GET','POST'])
+
+
+@spo.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
-    #test if is authenticated
+    # test if is authenticated
     if session['isAuthenticated'] == True:
-
-
-
         return render_template("dashboard.html")
 
+
 """
 +-------------------------------------------------+
 |                                                 |
-|                 Investigation                   |
+|             Company Investigation               |
 |                                                 |
 +-------------------------------------------------+
-Function that is responsible for start a new investigation.
+Function that is responsible for start a new Company investigation.
 """
-@spo.route("/investigation", methods=['GET','POST'])
-def investigation():
+@spo.route("/companyInvestigation", methods=['GET', 'POST'])
+def companyInvestigation():
     # test if is authenticated
     if session['isAuthenticated'] == True:
 
         if request.method == 'POST':
-            #start a new project
+            # start a new project
             nameOfProject = request.form.get('nameOfProject')
-            #send a informa
+            # send a informa
             if request.form.get('domain'):
-                #getDomain
+                # getDomain
                 domain_name = request.form.get('domain')
-                #get whois info
+                # get whois info
                 ownerDomainInfo = whois.owner(domain_name)
 
                 if ownerDomainInfo['ownerid'] is None:
                     domainInfo = ""
                 else:
-                    #if owner is cnpj
+                    # if owner is cnpj
                     if len(ownerDomainInfo['ownerid']) == 18:
-                        #get cnpj info
+                        # get cnpj info
                         domainInfo = cnpj.getCNPJ(ownerDomainInfo['ownerid'])
-                        #get process info
+                        # get process info
                         process = lawyer.jurisprudence(ownerDomainInfo['ownerid'])
-                        #treat cnpj
+                        # treat cnpj
                         TreatCNPJ = cnpj.treat(ownerDomainInfo['ownerid'])
                         subdomains = subdomain.getSubdomains(domain_name)
                     else:
                         domainInfo = ''
 
+                return render_template('companyInvestigation.html', subdomain=subdomains, domain_name=domain_name,
+                                       cnpj=TreatCNPJ, ownerDomainInfo=ownerDomainInfo, domainInfo=domainInfo,
+                                       jurisprudence=process)
+
+        return render_template("companyInvestigation.html", subdomain='', domain_name='', ownerDomainInfo='', domainInfo='',
+                               jurisprudence="", cnpj="")
+
+"""
++-------------------------------------------------+
+|                                                 |
+|            Personal Investigation               |
+|                                                 |
++-------------------------------------------------+
+Function that is responsible for start a new Personal investigation.
+"""
+@spo.route("/personalInvestigation", methods=['GET','POST'])
+def personalInvestigation():
+    # test if is authenticated
+    if session['isAuthenticated'] == True:
 
 
-
-                return render_template('investigation.html', subdomain=subdomains, domain_name=domain_name, cnpj=TreatCNPJ, ownerDomainInfo = ownerDomainInfo, domainInfo=domainInfo, jurisprudence=process )
-
-        return render_template("investigation.html", subdomain='', domain_name='', ownerDomainInfo='',domainInfo='', jurisprudence="", cnpj="" )
-
-
+        return render_template("personalInvestigation.html")
 
 """
 +-------------------------------------------------+
@@ -215,11 +238,14 @@ Function that is responsible for search a servers and services.
 Here we'll use the shodan key and others tools for search,
 and crawlers and etc.. 
 """
+
+
 @spo.route("/searchServers")
 def searchServers():
     # test if is authenticated
     if session['isAuthenticated'] == True:
         return render_template("searchServers.html")
+
 
 """
 +-------------------------------------------------+
@@ -231,11 +257,14 @@ Function that is responsible for search a person, on facebook
 instagram, twitter and whatever public data allow on internet 
 about especific people.
 """
+
+
 @spo.route("/personalTrace")
 def personalTrace():
     # test if is authenticated
     if session['isAuthenticated'] == True:
-        return render_template("personalTrace.html")
+        return render_template("personalInvestigation.html")
+
 
 """
 +-------------------------------------------------+
@@ -245,11 +274,14 @@ def personalTrace():
 +-------------------------------------------------+
 Function about educational articles
 """
+
+
 @spo.route("/aboutOSINT")
 def aboutOSINT():
     # test if is authenticated
     if session['isAuthenticated'] == True:
         return render_template("aboutOSINT.html")
+
 
 """
 +-------------------------------------------------+
@@ -259,6 +291,8 @@ def aboutOSINT():
 +-------------------------------------------------+
 Function for help
 """
+
+
 @spo.route("/manual")
 def manual():
     # test if is authenticated
@@ -275,11 +309,13 @@ def manual():
 Function that is responsible for insert a credentials
 for social media when we using for search on crawlers
 """
-@spo.route("/insertKeys", methods=['GET','POST'])
+
+
+@spo.route("/insertKeys", methods=['GET', 'POST'])
 def insertKeys():
     # test if is authenticated
     if session['isAuthenticated'] == True:
-        #test existent keys
+        # test existent keys
         if dao.getShodanKey(session['id']) is None:
             pass
         else:
@@ -289,16 +325,15 @@ def insertKeys():
 
             shodanKey = request.form.get('shodan')
             if dao.getShodanKey(session['id']) is None:
-                dao.insertShodanKey(session['id'],shodanKey)
-                flash('Shodan Key inserted: '+ dao.getShodanKey(session['id'])['key_value'], 'info')
+                dao.insertShodanKey(session['id'], shodanKey)
+                flash('Shodan Key inserted: ' + dao.getShodanKey(session['id'])['key_value'], 'info')
                 return redirect('insertKeys')
             else:
-                dao.updateShodanKey(session['id'],shodanKey)
-                flash('Shodan Key updated: '+ dao.getShodanKey(session['id'])['key_value'], 'info')
+                dao.updateShodanKey(session['id'], shodanKey)
+                flash('Shodan Key updated: ' + dao.getShodanKey(session['id'])['key_value'], 'info')
                 return render_template("insertKeys.html")
 
         return render_template("insertKeys.html")
-
 
 
 """
@@ -310,11 +345,14 @@ def insertKeys():
 The best function on this software, because here is
 a bitcoin wallet ♥ ♥ ♥ ♥ uheuheuheu 
 """
+
+
 @spo.route("/about")
 def aboutUS():
     # test if is authenticated
     if session['isAuthenticated'] == True:
         return render_template("aboutus.html")
+
 
 """
 +-------------------------------------------------+
@@ -331,5 +369,4 @@ if __name__ == '__main__':
     # spo run in 1337 port tcp
     # For real use disallow debug mode, set False
     spo.run(port=1337, debug=True)
-    __author__='Victor de Queiroz'
-
+    __author__ = 'Victor de Queiroz'
