2232ff2f40215eb8cbe86329300e3465ce451a3e
Victor de Queiroz <victordequeiroz37@gmail.com>
2018-05-09 13:43:17 -0300

    started a facebook crawler structure for search

---
+++ controller/Facebook.py
@@ -0,0 +1,71 @@
+__Author__ = 'Victor de Queiroz'
+"""
+Class for facebook search.
+On this class we can search a personal info from facebook
+
++-------------------------------------------------------+
+Profile search
+https://facebook.com/name.profile-search.example
++-------------------------------------------------------+
+About
+https://facebook.com/name.profile-search.example/about
+[*] overview
+[*] work and education
+[*] places he's lived
+[*] contacts and basic info
+[*] family and relationships
+[*] details about example
+[*] life events
++-------------------------------------------------------+
+Friends
+https://facebook.com/name.profile-search.example/friends
+* This topics can change *
+[*] All Friends
+[*] Mutual Friends
+[*] College 
+[*] High School
+[*] Current City
+[*] More
++-------------------------------------------------------+
+Likes
+https://facebook.com/name.profile-search.example/likes
++-------------------------------------------------------+
+Photos
+https://facebook.com/name.profile-search.example/photos
++-------------------------------------------------------+
+Videos
+https://facebook.com/name.profile-search.example/videos
++-------------------------------------------------------+
+CheckIns
+https://facebook.com/name.profile-search.example/map
++-------------------------------------------------------+
+Sports
+https://facebook.com/name.profile-search.example/sports
++-------------------------------------------------------+
+Music
+https://facebook.com/name.profile-search.example/music
++-------------------------------------------------------+
+TV
+https://facebook.com/name.profile-search.example/tv
++-------------------------------------------------------+
+Movies
+https://facebook.com/name.profile-search.example/movies
++-------------------------------------------------------+
+Books
+https://facebook.com/name.profile-search.example/books
++-------------------------------------------------------+
+Events
+https://facebook.com/name.profile-search.example/events
++-------------------------------------------------------+
+
+
++-------------------------------------------------------+
+Search post about company
+https://www.facebook.com/search/str/<name+of+company>/keywords_blended_posts
++-------------------------------------------------------+
+
+
+
+
+"""
+
